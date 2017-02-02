833d73f7fd77bf4adb0e20d654151fe054439c1e
Matthew Mcclatchie <matt@first10.co.uk>
2017-02-02 15:49:18 +0000

    Moved simple_toggle JS to scripts.js file and included in footer

---
+++ templates/assets/js/scripts.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// simple_toggler.js
+
+function simple_toggler( trigger, target ) {
+
+	// If either argument is undefined, end here.
+  if ( trigger === undefined || target === undefined ) {
+  	return false;
+  }
+
+  // Set up internal variables to contain arguments.
+	var tr = document.querySelector( trigger ), 
+			ta = document.querySelector( target ) ;
+
+
+	// Event handlder
+	tr.addEventListener( 'click', function() {
+
+		// If the trigger has a class of triggered
+		if ( tr.classList.contains( 'triggered' ) ) {
+
+			// Remove that class
+			this.classList.remove( 'triggered' );
+			
+			// Set the target height to 0
+			ta.style.height = 0 + 'px';
+			// Remove visible class
+			ta.classList.remove( 'visible' );
+
+		} else {
+
+			// Add triggered class
+			this.classList.add( 'triggered' );
+
+			// Set the target height to it's scroll height
+			ta.style.height = ta.scrollHeight + 'px';
+			// Add visible class
+			ta.classList.add( 'visible' );
+
+		}
+	});
+}
+
+
+
+// Scripts
+
+simple_toggler( '#gs-nav-trigger', '.gs-nav__nav' )
