9df6e55f98061d481f58bf8ba8357e31a5b07b8a
Matthew Mcclatchie <matt@first10.co.uk>
2017-02-21 15:32:50 +0000

    Added JS to Progress element.
    - Set up Object to control displaying of a value
    - Set up a Method to allow updating of the Object's properties and trigger a visual update.

--- templates/assets/js/scripts.js
+++ templates/assets/js/scripts.js
@@ -1,3 +1,128 @@
-/* Progress JS */
-var test = 'progress JS';
+/**
+ *
+ * File Progress.js.
+ *
+ */
 
+$(function () {
+	"use strict";
+
+  // @todo 1 add wrappers property to all dropdowns after ready class is added
+  // This will aid in hiding all dropdowns when a new one is hovered on
+  // DO THE SAME FOR TRIGGERS
+
+  // @todo update hideDropdown method to accept more targeted arguments (hide single or hide all)
+
+  // @todo [method] add option to decide if dropdown overlays or pushes content down
+
+  // @todo add option to hover or click
+
+  // @todo Don't really need settings.wrapper (SINGULAR) because this is the element. Maybe this can be passed to methods that need it?
+
+	var Progress = window.Progress || {};
+
+  Progress = (function() {
+  	function Progress( element, settings ) {
+
+      // The element
+  		this.progress = ( element );
+  		// The Progress element
+  		this.progressElem = $( element ).children( 'progress' );
+  		// The Fallback Span
+  		this.progressFall = $( '.rc-progress--fallback__value' );
+
+			// Setup Object to contain properties related to the value
+			this.valueProps = {
+				"class" : [
+					"rc-progress__value"
+				],
+				"percent" : ""
+			}
+
+  		this.init();
+  	}
+  	return Progress;
+  }());
+
+
+  Progress.prototype.valueAsPercentage = function( number ) {
+  	return number + '%';
+  }
+
+  Progress.prototype.init = function( element ) {
+    // Ready
+    $( this.progress ).addClass( 'rc-progress-init' );
+    // Trigger method
+    this.getValue();
+  }
+
+  Progress.prototype.getValue = function( element ) {
+  	// Get the data attribute value
+  	this.progressValue = $( this.progress ).data( 'value' );
+  	
+  	// Set property to vaue
+  	this.valueProps.percent = this.valueAsPercentage( this.progressValue );
+
+  	// Trigger method
+  	this.prepareValue();
+  }
+
+  Progress.prototype.updateValue = function( newValue ) {
+
+		this.valueProps.percent = this.valueAsPercentage( newValue );
+		this.progress.data( 'value', newValue );
+		this.progress.attr( 'data-value', newValue );
+		this.progressElem[0].value = newValue;
+		this.progressFall.css('width', this.valueAsPercentage( newValue ) );
+
+		$( '.rc-progress__value' ).remove();
+
+		this.displayValue();
+  }
+
+
+	Progress.prototype.prepareValue = function( element ) {
+		// If the number is less than 10, add another class to the class array
+		if ( 10 > this.progressValue ) {
+			this.valueProps.class.push( 'rc-progress__value--outside' );
+		}
+
+		this.displayValue();
+	}
+
+
+
+  Progress.prototype.displayValue = function( element ) {
+
+  	this.valueContainer = '<div class="' + this.valueProps.class.join( ' ' ) + '" style="left: ' + this.valueProps.percent + '">' + this.valueProps.percent + '</div>';
+  	$( this.progress ).append( this.valueContainer );
+  }
+
+
+
+  // jQuery Prototpe Function
+	$.fn.Progress = function() {
+		var progress = new Progress( this );
+		
+		this.updateProgress = function( newValue ){
+			progress.updateValue( newValue );
+		};
+
+		return this;
+	};
+
+
+	var prog = $('.rc-progress');
+
+	// Selector
+	prog.Progress();
+
+
+
+	// Update value test
+  $( '.gs-stage__content h1').on('click', function( event ) {
+  	// Update Value
+  	prog.updateProgress( 20 );
+  });
+
+});
