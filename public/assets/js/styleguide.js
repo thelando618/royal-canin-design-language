95dc1151c7218d5355ccf081850903a1084d87ac
Matthew Mcclatchie <matt@first10.co.uk>
2017-02-22 15:38:54 +0000

    Removed na.js JS file as is redundant. Moved GS scripts to styleguide.js file temporarily, until they are replaced by more appropriate component JS.

--- public/assets/js/styleguide.js
+++ public/assets/js/styleguide.js
@@ -1,3 +1,121 @@
+'use strict';
+
+
+// simple_toggler.js
+
+function simple_toggler( trigger, target ) {
+
+	// If either argument is undefined, end here.
+  if ( trigger === undefined || target === undefined ) {
+  	return false;
+  }
+
+  // Set up internal variables to contain arguments.
+	var tr = document.querySelector( trigger ), 
+			ta = document.querySelector( target ) ;
+
+
+	// Event handlder
+	tr.addEventListener( 'click', function() {
+
+		// If the trigger has a class of triggered
+		if ( tr.classList.contains( 'triggered' ) ) {
+
+			// Remove that class
+			this.classList.remove( 'triggered' );
+			
+			// Set the target height to 0
+			// ta.style.height = 0 + 'px';
+			// Remove visible class
+			ta.classList.remove( 'visible' );
+
+		} else {
+
+			// Add triggered class
+			this.classList.add( 'triggered' );
+
+			// Set the target height to it's scroll height
+			// ta.style.height = ta.scrollHeight + 'px';
+			// Add visible class
+			ta.classList.add( 'visible' );
+
+		}
+	});
+}
+
+
+
+
+// nav_accordion.js
+
+function nav_accordion( accordion, trigger ) {
+	var a = document.querySelector( accordion );
+	var ts =	document.querySelectorAll( trigger );
+
+
+	ts.forEach( function( t ) {
+		t.addEventListener( 'mousedown', function( event ) {
+			event.preventDefault();
+
+			if ( t.classList.contains( 'triggered' ) ) {
+
+				t.classList.remove( 'triggered' );
+				t.parentNode.querySelector('ul').classList.remove( 'visible' );
+
+			} else {
+
+				t.classList.add( 'triggered' );
+				t.parentNode.querySelector('ul').classList.add( 'visible' );
+			}
+
+		} );
+
+		t.addEventListener( 'focus', function( event ) {
+			event.preventDefault();
+
+			if ( t.classList.contains( 'triggered' ) ) {
+
+				t.classList.remove( 'triggered' );
+				t.parentNode.querySelector('ul').classList.remove( 'visible' );
+
+			} else {
+
+				t.classList.add( 'triggered' );
+				t.parentNode.querySelector('ul').classList.add( 'visible' );
+			}
+
+		} );
+
+	});
+
+}
+
+
+
+// code_to_text.html
+
+// function htmlEntities(str) {
+// 	return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+// }
+
+// var the_codes = document.querySelectorAll('.gs-code');
+
+// the_codes.forEach( function( code ) {
+
+
+// 	// var test = code.innerHTML.trim();
+
+// 	var html = code.innerHTML.trim();
+// 	html = html.replace(/\s/g, '\n');
+// 	html = html.replace(/\n/g, '\n');
+
+
+// 	code.innerHTML = htmlEntities( html );
+// } );
+
+
+
+
 // Convert HTML Tags to Entities
 function htmlEntities(str) {
 	return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
@@ -29,5 +147,41 @@ function syntax( code_block ) {
 		the_code.innerHTML = htmlEntities( the_code.innerHTML.trim() );
 	} );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var the_codes = document.querySelectorAll('.gs-code');
+
+// the_codes.forEach( function( the_code ) {
+
+// 	var inside = the_code.innerHTML.replace(/\t/g, '');
+// 	console.log( inside );
+// 	the_code.innerHTML = htmlEntities( inside );
+
+// 	//the_code.innerHTML = the_codes = the_codes.replace(/\t/g, '');
+
+// } );
+
+// the_codes = the_codes.replace(/\t/g, '');
+// console.log( the_codes );
+
+
+
+
+// Scripts
+
+simple_toggler( '#gs-nav-trigger', '.gs-nav__nav' );
+
+nav_accordion( '.gs-nav__nav', '.gs-nav__trigger');
 
 syntax( '.gs-code ');
