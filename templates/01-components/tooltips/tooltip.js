77072300f2b1b105b06cac87ce71691193ebdf65
Matthew Mcclatchie <matt@first10.co.uk>
2017-02-27 15:16:10 +0000

    Amended JS to work with static or following tooltips.
    - Added data attribute to determine static or following.
    - Style amends.

--- templates/01-components/tooltips/tooltip.js
+++ templates/01-components/tooltips/tooltip.js
@@ -21,14 +21,14 @@
   }());
 
 
-
   Tooltips.prototype.init = function( element ) {
     // Ready
     $( this.tooltip ).addClass( 'rc-tooltip-init' );
 
+    // Get the tooltip arrows
     this.arrow = $( '.rc-tooltip__arrow' );
-    console.log( this.arrow );
 
+    // Trigger method
     this.event();
   }
 
@@ -37,35 +37,70 @@
   	var th = this;
 
     $( this.tooltip ).on( 'mouseenter focus', function( event ) {
-      var tooltip = $( this ).data('tooltip' );
-      th.showTooltip( $( '#' + tooltip ) );
+      var tooltip = $( this ).data( 'tooltip' );
+      th.showTooltip( $( '#' + tooltip ), event );
     });
 
     $( this.tooltip ).on( 'mousemove', function( event, pos ) {
-      var tooltip = $( this ).data('tooltip' );
-      th.moveTooltip( $( '#' + tooltip ), event );
-    });
+      var tooltip = $( this ).data( 'tooltip' );
+    	var follow = $( '#' + tooltip ).data( 'tooltipFollow' );
 
+    	if ( true === follow ) {
+    		th.moveTooltip( $( '#' + tooltip ), event );
+    	}
+    });
 
     $( this.tooltip ).on( 'mouseout blur', function( event ) {
-      var tooltip = $( this ).data('tooltip' );
-      th.hideTooltip( $( '#' + tooltip ) );
+      var tooltip = $( this ).data( 'tooltip' );
+			th.delay = setTimeout( function() {
+				th.hideTooltip( $( '#' + tooltip ) );
+    	}, 150);
     });
+
+
+    $( '.rc-tooltip' ).hover(
+      function( event ) {
+    	 	clearTimeout( th.delay );
+      }, function() {
+		    var tooltip = $( this ).data( 'tooltip' );
+      	th.hideTooltip( $( '.rc-tooltip' ) );
+      }
+    );
   }
 
 
-  Tooltips.prototype.showTooltip = function( tooltip ) {
+  Tooltips.prototype.showTooltip = function( tooltip, event ) {
+  	var border_top = $(window).scrollTop();
+  	var border_right = $(window).width();
+    var left_pos;
+    var top_pos;
+    var offset = 16;
+
+		if( border_right - ( offset * 2 ) >= tooltip.outerWidth() + event.pageX ) {
+			left_pos = event.pageX - offset * 0.25;
+		} else {
+			left_pos = border_right - tooltip.outerWidth() - offset * 2;
+		}
+
+		if( border_top + ( offset *2 ) >= event.pageY - tooltip.height() ) {
+			this.arrow.addClass( 'rc-tooltip__arrow--top' );
+			top_pos = event.pageY + offset;
+		} else {
+			this.arrow.removeClass( 'rc-tooltip__arrow--top' );
+			top_pos = event.pageY - tooltip.height() - offset * 2;
+		}
+
   	tooltip.css({
   		display: 'none',
   		right: 'auto', 
   		opacity: 1,
-  		top: tooltip.pageY + 16,
-  		left: tooltip.pageX - 32
-  	}).fadeIn( 400 );
+  		top: top_pos,
+  		left: left_pos
+  	}).stop( true, true ).fadeIn( 400 );
   }
 
-  Tooltips.prototype.moveTooltip = function( tooltip, event ) {
 
+  Tooltips.prototype.moveTooltip = function( tooltip, event ) {
   	var border_top = $(window).scrollTop();
   	var border_right = $(window).width();
     var left_pos;
@@ -91,6 +126,7 @@
   		left: left_pos
   	});
   }
+
 
   Tooltips.prototype.hideTooltip = function( tooltip ) {
   	tooltip.fadeOut(400);
