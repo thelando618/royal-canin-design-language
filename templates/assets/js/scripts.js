c6ece93aaf05f937f0e64229bc7a048524c6422f
Matthew Mcclatchie <matt@first10.co.uk>
2017-03-10 14:45:12 +0000

    Rebuilt portal with redundant sliders file removed

--- templates/assets/js/scripts.js
+++ templates/assets/js/scripts.js
@@ -191,6 +191,147 @@ $(function () {
 ;(function ( $, window, document, undefined ) {
 
 	"use strict";
+
+	var Tabs = window.Tabs || {};
+
+  Tabs = (function() {
+  	function Tabs( element ) {
+      // The element
+  		this.tabs = ( element );
+
+      // Settings
+      this.settings = {
+        "initClass"     : "rc-tabs-init",
+        "triggerClass"  : ".rc-tabs__triggers__trigger",
+        "contentClass"  : ".rc-tabs__content__single",
+        "firstShowing"  : false
+      }
+
+      // Triggers
+      this.triggers = $( this.tabs ).find( this.settings.triggerClass );
+
+      // Content
+      this.content = $( this.tabs ).find( this.settings.contentClass );
+
+  		this.init();
+  	}
+  	return Tabs;
+  }());
+
+
+  Tabs.prototype.init = function( element ) {
+    // Ready
+    $( this.tabs ).addClass( this.settings.initClass );
+
+    // Hide all Content
+    this.content.hide();
+
+    // Trigger count method
+    this.count();
+
+    // Trigger showFirst method
+    this.showFirst();
+
+    // Once showFirst method has completed and firstShowing property has been updated, invoke the triggerEvent method
+    if ( true === this.settings.firstShowing ) {
+      // Call triggerEvent method which has eventListeners in for future clicks
+      this.triggerEvent();
+    }
+  }
+
+
+  Tabs.prototype.count = function( element ) {
+    // For each content block, if there is no accompanying trigger, remove it from the DOM.
+    for ( var i = 0; i < this.content.length; i++ ) {
+      if ( $( this.content[i] ).attr( 'id' ) !== $( this.triggers[i] ).data( 'tab' ) ) {
+        $( this.content[i] ).remove();
+      }
+    }
+
+    // For each trigger, if there is no accompanying content block, remove it from the DOM.
+    for ( var i = 0; i < this.triggers.length; i++ ) {
+      if ( $( this.content[i] ).attr( 'id' ) !== $( this.triggers[i] ).data( 'tab' ) ) {
+        $( this.triggers[i] ).parent( 'li' ).remove();
+      }
+    }
+  }
+
+
+  Tabs.prototype.showFirst = function( element ) {
+    // Get first trigger
+    var firstTrigger = $( this.triggers.first() );
+    // Set first trigger to active
+    firstTrigger.addClass( 'active' );
+    // Get data-tab attribute from first trigger
+    var triggerData = firstTrigger.attr( 'data-tab' );
+    // Call changeTab method with first trigger data attribute passed
+    this.changeTab( triggerData );
+  }
+
+
+  Tabs.prototype.changeTab = function( tab ) {
+    // Hide all content
+    $( this.content ).hide();
+    // Show content with ID matching argument
+    $( this.settings.contentClass + "#" + tab ).show();
+
+    // If this is the first time this method has been run, update a property
+    // This will enable further methods to be able to be invoked
+    if ( false === this.settings.firstShowing ) {
+      this.settings.firstShowing = true;
+    }
+  }
+
+
+  Tabs.prototype.triggerEvent = function( element ) {
+    // This
+    var th = this;
+
+    $( this.triggers ).on('click', function( event ) {
+      event.preventDefault();
+
+      if ( $( this ).hasClass('active') ) {
+        return false;
+      } else {
+        $( th.triggers ).removeClass( 'active' );
+        $( this ).addClass( 'active' );
+
+        var triggerData = $( this ).attr( 'data-tab' );
+        th.changeTab( triggerData );
+
+      }
+    });
+  }
+
+
+  // Initiate all Tabs Elements.
+	$.fn.Tabs = function () {
+		return this.each( function () {
+			var tabs = new Tabs( this );
+		});
+	};
+
+})( jQuery, window, document );
+
+
+
+
+
+
+// Move this to Scripts
+$(function () {
+	$('.rc-tabs').Tabs(); // Initiate all Tabs elements
+});
+
+/**
+ *
+ * File Tabs.js.
+ *
+ */
+
+;(function ( $, window, document, undefined ) {
+
+	"use strict";
 	
 	var Tooltips = window.Tooltips || {};
 
@@ -335,145 +476,4 @@ $(function () {
 // Move this to Scripts
 $(function () {
 	$( '.rc-tooltip__trigger' ).Tooltips(); // Initiate all Tabs elements
-});
-
-/**
- *
- * File Tabs.js.
- *
- */
-
-;(function ( $, window, document, undefined ) {
-
-	"use strict";
-
-	var Tabs = window.Tabs || {};
-
-  Tabs = (function() {
-  	function Tabs( element ) {
-      // The element
-  		this.tabs = ( element );
-
-      // Settings
-      this.settings = {
-        "initClass"     : "rc-tabs-init",
-        "triggerClass"  : ".rc-tabs__triggers__trigger",
-        "contentClass"  : ".rc-tabs__content__single",
-        "firstShowing"  : false
-      }
-
-      // Triggers
-      this.triggers = $( this.tabs ).find( this.settings.triggerClass );
-
-      // Content
-      this.content = $( this.tabs ).find( this.settings.contentClass );
-
-  		this.init();
-  	}
-  	return Tabs;
-  }());
-
-
-  Tabs.prototype.init = function( element ) {
-    // Ready
-    $( this.tabs ).addClass( this.settings.initClass );
-
-    // Hide all Content
-    this.content.hide();
-
-    // Trigger count method
-    this.count();
-
-    // Trigger showFirst method
-    this.showFirst();
-
-    // Once showFirst method has completed and firstShowing property has been updated, invoke the triggerEvent method
-    if ( true === this.settings.firstShowing ) {
-      // Call triggerEvent method which has eventListeners in for future clicks
-      this.triggerEvent();
-    }
-  }
-
-
-  Tabs.prototype.count = function( element ) {
-    // For each content block, if there is no accompanying trigger, remove it from the DOM.
-    for ( var i = 0; i < this.content.length; i++ ) {
-      if ( $( this.content[i] ).attr( 'id' ) !== $( this.triggers[i] ).data( 'tab' ) ) {
-        $( this.content[i] ).remove();
-      }
-    }
-
-    // For each trigger, if there is no accompanying content block, remove it from the DOM.
-    for ( var i = 0; i < this.triggers.length; i++ ) {
-      if ( $( this.content[i] ).attr( 'id' ) !== $( this.triggers[i] ).data( 'tab' ) ) {
-        $( this.triggers[i] ).parent( 'li' ).remove();
-      }
-    }
-  }
-
-
-  Tabs.prototype.showFirst = function( element ) {
-    // Get first trigger
-    var firstTrigger = $( this.triggers.first() );
-    // Set first trigger to active
-    firstTrigger.addClass( 'active' );
-    // Get data-tab attribute from first trigger
-    var triggerData = firstTrigger.attr( 'data-tab' );
-    // Call changeTab method with first trigger data attribute passed
-    this.changeTab( triggerData );
-  }
-
-
-  Tabs.prototype.changeTab = function( tab ) {
-    // Hide all content
-    $( this.content ).hide();
-    // Show content with ID matching argument
-    $( this.settings.contentClass + "#" + tab ).show();
-
-    // If this is the first time this method has been run, update a property
-    // This will enable further methods to be able to be invoked
-    if ( false === this.settings.firstShowing ) {
-      this.settings.firstShowing = true;
-    }
-  }
-
-
-  Tabs.prototype.triggerEvent = function( element ) {
-    // This
-    var th = this;
-
-    $( this.triggers ).on('click', function( event ) {
-      event.preventDefault();
-
-      if ( $( this ).hasClass('active') ) {
-        return false;
-      } else {
-        $( th.triggers ).removeClass( 'active' );
-        $( this ).addClass( 'active' );
-
-        var triggerData = $( this ).attr( 'data-tab' );
-        th.changeTab( triggerData );
-
-      }
-    });
-  }
-
-
-  // Initiate all Tabs Elements.
-	$.fn.Tabs = function () {
-		return this.each( function () {
-			var tabs = new Tabs( this );
-		});
-	};
-
-})( jQuery, window, document );
-
-
-
-
-
-
-// Move this to Scripts
-$(function () {
-	$('.rc-tabs').Tabs(); // Initiate all Tabs elements
 });
