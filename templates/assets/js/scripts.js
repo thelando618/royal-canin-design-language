c4b50b5a7f3ae6d847ae70e6870797136e09afa3
Matt McClatchie <first10@192.168.1.103>
2017-05-11 15:02:26 +0100

    Setup files for components/tooltip-popout

--- templates/assets/js/scripts.js
+++ templates/assets/js/scripts.js
@@ -480,6 +480,45 @@ $(function () {
 
 /**
  *
+ * File tooltips-popout.js.
+ *
+ */
+
+
+var tooltip_popout = {
+  init: function(  ) {
+
+  },
+
+  positioning: function(  ) {
+    
+  },
+
+  open_tooltip: function(  ) {
+
+  },
+
+  close_tooltip: function(  ) {
+
+  },
+}
+
+
+
+
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+
+ready( tooltip_popout.init( ) );
+
+/**
+ *
  * File Tabs.js.
  *
  */
