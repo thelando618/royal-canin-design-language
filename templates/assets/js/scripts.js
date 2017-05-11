2eb7c19884bd9b6250a7e7b85733981174c74d60
Matt McClatchie <first10@192.168.1.103>
2017-05-11 09:00:03 +0100

    Continued work on Maps.js file.
    - Maps + Markers can now be generated with a set of functions.

--- templates/assets/js/scripts.js
+++ templates/assets/js/scripts.js
@@ -574,29 +574,10 @@ $(function () {
  */
 
 
-
-// 
-// BEGIN - global - do not edit
-// -------------------------------------------------------------------------------------------------------------------------------------------- //
-// 
-
-
-var key_locations = {
-  royal_canin: {
-    lat: 43.700753,
-    lng: 4.187961
-  },
-  first_10: {
-    lat: 53.790639,
-    lng: -1.532333
-  },
-  first_10_2: {
-    lat: 53.790639,
-    lng: -1.532
-  }
-}
-
-// Map icon
+/**
+ * Custom Map icon SVG settings
+ * @type {object}
+ */
 var icon = {
   path: 'M32 0C14.3 0 0 14.3 0 32s14.3 32 32 32 32-14.3 32-32S49.7 0 32 0zm0 57C18.2 57 7 45.8 7 32S18.2 7 32 7s25 11.2 25 25-11.2 25-25 25zm11.5-25c0 6.4-5.1 11.5-11.5 11.5S20.5 38.4 20.5 32 25.6 20.5 32 20.5 43.5 25.6 43.5 32z',
   fillColor: '#E2001A',
@@ -606,69 +587,118 @@ var icon = {
 }
 
 /**
- * Create a Google Maps Marker
- * @param  object   position  The desired position of the Marker
- * @param  object   map       The Map to apply the Marker to      
- * @return object             The Marker applied to the Map
+ * Creates a Google Map
+ * @param  {object} selector The DOM ID to set up the Map in
+ * @param  {object} center   The desired center of the Map
+ * @param  {number} zoom     The zoom level of the Map
+ * @return {object}          The Map
  */
-function create_marker ( position, map ) {
-  if ( typeof map === "undefined" || typeof position === "undefined") {
+function create_map ( selector, center, zoom ) {
+  /**
+   * Sets default number for zoom
+   * @type {number}
+   */
+  var zoom = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 17;
+
+  /**
+   * Exit the function if any of the arguments passed are empty
+   * @return {bool} false
+   */
+  if ( null === selector || typeof center === 'undefined' || typeof zoom === 'undefined' ) {
     return false;
   }
 
+  /**
+   * Creates the new map with arguments passed and some defaults
+   * @type {google}
+   */
+  var new_map = new google.maps.Map( selector, {
+    center: center,
+    scrollwheel: false,
+    zoom: zoom
+  } );
+
+  return new_map;
+}
+
+/**
+ * Create a Google Maps Marker
+ * @param  {object}   position  The desired position of the Marker
+ * @param  {object}   map       The Map to apply the Marker to      
+ * @return {object}             The Marker applied to the Map
+ */
+function create_marker ( position, map ) {
+  /**
+   * Exit the function if any of the arguments passed are empty
+   * @return {bool} false
+   */
+  if ( typeof position === 'undefined' || typeof map === 'undefined', false === map ) {
+    return false;
+  }
+
+  /**
+   * Creates the new map marker with arguments passed
+   * @type {google}
+   */
   var map_marker = new google.maps.Marker({
     icon: icon,
     position: position,
     map: map
   });
 
-  // return not strictly needed, as Marker gets applied when new Method is called.
   return map_marker;
 }
 
 
+
+
+
+
+
+
 // 
-// END - global - do not edit
+// Following would live in custom scripts file
 // -------------------------------------------------------------------------------------------------------------------------------------------- //
 // 
 
-
-
-
-// Settings
-var settings_map_royal_canin = {
-  center: key_locations.royal_canin,
-  scrollwheel: false,
-  zoom: 17
+/**
+ * Object containing lat/lng of key locations
+ * @type {object}
+ */
+var key_locations = {
+  royal_canin: {
+    lat: 43.700753,
+    lng: 4.187961
+  },
+  first_10: {
+    lat: 53.790639,
+    lng: -1.532333
+  }
 }
-
-var settings_map_first_10 = {
-  center: key_locations.first_10,
-  scrollwheel: false,
-  zoom: 18
-}
-
 
 
 /**
  * Standard Google Maps init function. Generates Maps.
  */
 function initMap() {
+  // Royal Canin Map + Marker
+  var map_royal_canin = create_map( document.getElementById( 'map_royal_canin' ), key_locations.royal_canin, 17 );
+  create_marker( key_locations.royal_canin, map_royal_canin );
 
-  var dom_map_royal_canin = document.getElementById( 'map_royal_canin' );
-  var dom_map_first_10 = document.getElementById( 'map_first_10' );
-
-  if ( null !== dom_map_royal_canin ) {
-    var map_royal_canin = new google.maps.Map( dom_map_royal_canin, settings_map_royal_canin );
-    create_marker( key_locations.royal_canin, map_royal_canin );
-  }
-
-  if ( null !== dom_map_first_10 ) {
-    var map_first_10 = new google.maps.Map( dom_map_first_10, settings_map_first_10 );
-    create_marker( key_locations.first_10, map_first_10 );
-    create_marker( key_locations.first_10_2, map_first_10 );
-  }
+  // First 10 Map + Marker
+  var map_first_10 = create_map( document.getElementById( 'map_first_10' ), key_locations.first_10, 17 );
+  create_marker( key_locations.first_10, map_first_10 );
 
 }
+
+
+
+
+/*
+
+  MORNING!
+  - MAP WITH OVERLAY
+ */
+
 
 
