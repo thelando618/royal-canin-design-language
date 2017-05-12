52f4a80fdb0e62fb631c8d38b465211c59e3477f
Matt McClatchie <first10@192.168.1.103>
2017-05-12 07:55:16 +0100

    Setup demo of tooltip that is triggered by button and contains list.
    Uses exisitng tooltip JS with a few style adjustments.
    
    Setup new button partial that allows a data attribute to be passed.

--- templates/assets/js/scripts.js
+++ templates/assets/js/scripts.js
@@ -480,50 +480,6 @@ $(function () {
 
 /**
  *
- * File tooltips-popout.js.
- *
- */
-
-
-var tooltip_popout = {
-
-  init: function( wrapper ) {
-
-    console.log( wrapper );
-
-  },
-
-  positioning: function( test ) {
-    
-    console.log( test );
-
-  },
-
-  open_tooltip: function(  ) {
-
-  },
-
-  close_tooltip: function(  ) {
-
-  },
-}
-
-
-
-
-function ready(fn) {
-  if (document.readyState != 'loading'){
-    fn();
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
-  }
-}
-
-
-ready( tooltip_popout.init( 'rc-tooltip--popout' ) );
-
-/**
- *
  * File Tabs.js.
  *
  */
