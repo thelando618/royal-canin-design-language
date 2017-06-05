b81db61c61bc9ddf67e2a59269fd3db10c1f5bb2
James Drawneek <jmdrawneek@googlemail.com>
2017-06-05 15:23:33 +0100

    https://trello.com/c/udlDLUvM/164-nav-link-hover-text-correct-ref. Removed rollover blue colour from navigation links.
    https://trello.com/c/m2HjULYb/186-background-colour-grey-to-be-%23f3f3f3-on-%27tabs-%3E-tab-example%27.
    Added abstraction and start of separation of styles and layout.

--- templates/assets/js/scripts.js
+++ templates/assets/js/scripts.js
@@ -622,57 +622,6 @@ $(function() {
 
 /**
  *
- * File sliders.js.
- *
- */
-
-
-/**
- * Checks if document is ready
- * @param  {Function} fn Whatever is passed in the ready function 
- */
-function ready(fn) {
-  if (document.readyState != 'loading'){
-    fn();
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
-  }
-}
-
-
-
-
-
-
-// @todo - Will live in scripts file
-// Native document ready
-ready( function(  ) {
-
-	// Get element
-	var range = document.getElementById( 'rc-slider-demo' );
-
-	// Create Slider
-	noUiSlider.create( range, {
-		start: [ 0 ],
-		connect: [true, false],
-		behaviour: 'tap-drag', 
-		step: 10,
-		tooltips: true,
-		range: {
-			'min': 0,
-			'max': 100
-		},
-		pips: { 
-			mode: 'steps',
-			stepped: true,
-			density: 2
-		}
-	});
-
-
-} );
-/**
- *
  * File Progress.js.
  *
  */
@@ -803,6 +752,57 @@ $(function () {
 
 
 });
+/**
+ *
+ * File sliders.js.
+ *
+ */
+
+
+/**
+ * Checks if document is ready
+ * @param  {Function} fn Whatever is passed in the ready function 
+ */
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+
+
+
+
+
+// @todo - Will live in scripts file
+// Native document ready
+ready( function(  ) {
+
+	// Get element
+	var range = document.getElementById( 'rc-slider-demo' );
+
+	// Create Slider
+	noUiSlider.create( range, {
+		start: [ 0 ],
+		connect: [true, false],
+		behaviour: 'tap-drag', 
+		step: 10,
+		tooltips: true,
+		range: {
+			'min': 0,
+			'max': 100
+		},
+		pips: { 
+			mode: 'steps',
+			stepped: true,
+			density: 2
+		}
+	});
+
+
+} );
 /**
  *
  * File Tabs.js.
