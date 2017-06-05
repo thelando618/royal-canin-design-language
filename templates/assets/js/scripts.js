c4232ca69e1c146a3a4c3671afccfe20dd00fdab
James Drawneek <jmdrawneek@googlemail.com>
2017-06-05 11:29:44 +0100

    Added section type for featured items. Restricted width of single cards. https://trello.com/c/vj15mNUa/142-max-width-of-310px-on-cards-and-left-align

--- templates/assets/js/scripts.js
+++ templates/assets/js/scripts.js
@@ -823,56 +823,6 @@ $(function () {
 
 /**
  *
- * File image-galleries.js.
- *
- */
-
-$(function () {
-
-  // Carousel Settings
-  var carousel_next_arrow = '<a href="#" class="interactive--navigation interactive--navigation--next"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35.5 64" aria-labelledby="svg-arrow-right" role="img"><title id="svg-arrow-right" lang="en">arrow-right</title><path d="M34.5 34.5L6 63c-.7.7-1.6 1-2.5 1s-1.8-.3-2.5-1c-1.4-1.4-1.4-3.6 0-5l26-26L1 6C-.4 4.6-.4 2.4 1 1S4.6-.4 6 1l28.5 28.5c1.4 1.4 1.4 3.6 0 5z"></path></svg><span class="screen-reader-text">Next Slide</span></a>',
-      carousel_prev_arrow = '<a href="#" class="interactive--navigation interactive--navigation--prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35.5 64" aria-labelledby="svg-arrow-left" role="img"><title id="svg-arrow-left" lang="en">arrow-left</title><path d="M1 29.5L29.5 1c1.4-1.4 3.6-1.4 5 0s1.4 3.6 0 5l-26 26 26 26c1.4 1.4 1.4 3.6 0 5-.7.7-1.6 1-2.5 1-.9 0-1.8-.3-2.5-1L1 34.5c-1.3-1.4-1.3-3.6 0-5z"></path></svg><span class="screen-reader-text">Previous Slide</span></a>';
-
-  var carousel_gallery_main = {
-    adaptiveHeight: true,
-    asNavFor: '.rc-carousel--gallery__thumbs',
-    autoplay: true,
-    nextArrow: carousel_next_arrow,
-    prevArrow: carousel_prev_arrow,
-    slidesToShow: 1
-  }
-
-  var carousel_gallery_thumbs = {
-    adaptiveHeight: true,
-    arrows: false,
-    asNavFor: '.rc-carousel--gallery__main',
-    focusOnSelect: true,
-    mobileFirst: true,
-    nextArrow: carousel_next_arrow,
-    prevArrow: carousel_prev_arrow,
-    slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 640,
-        settings: {
-          slidesToShow: 5
-        }
-      }
-    ]
-  }
-
-  // Carousel Caches
-  var $slick_gallery = $( '.rc-carousel--gallery__main' );
-  var $slick_thumbs = $( '.rc-carousel--gallery__thumbs' );
-
-  // Carousel Inits
-  $slick_gallery.slick( carousel_gallery_main );
-  $slick_thumbs.slick( carousel_gallery_thumbs );
-
-});
-
-/**
- *
  * File maps.js.
  *
  */
@@ -1120,3 +1070,53 @@ function initMap() {
   var infobox_first_10 = create_infobox( document.getElementById( 'infobox_first_10' ), marker_first_10, map_first_10 );
 }
 
+
+/**
+ *
+ * File image-galleries.js.
+ *
+ */
+
+$(function () {
+
+  // Carousel Settings
+  var carousel_next_arrow = '<a href="#" class="interactive--navigation interactive--navigation--next"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35.5 64" aria-labelledby="svg-arrow-right" role="img"><title id="svg-arrow-right" lang="en">arrow-right</title><path d="M34.5 34.5L6 63c-.7.7-1.6 1-2.5 1s-1.8-.3-2.5-1c-1.4-1.4-1.4-3.6 0-5l26-26L1 6C-.4 4.6-.4 2.4 1 1S4.6-.4 6 1l28.5 28.5c1.4 1.4 1.4 3.6 0 5z"></path></svg><span class="screen-reader-text">Next Slide</span></a>',
+      carousel_prev_arrow = '<a href="#" class="interactive--navigation interactive--navigation--prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35.5 64" aria-labelledby="svg-arrow-left" role="img"><title id="svg-arrow-left" lang="en">arrow-left</title><path d="M1 29.5L29.5 1c1.4-1.4 3.6-1.4 5 0s1.4 3.6 0 5l-26 26 26 26c1.4 1.4 1.4 3.6 0 5-.7.7-1.6 1-2.5 1-.9 0-1.8-.3-2.5-1L1 34.5c-1.3-1.4-1.3-3.6 0-5z"></path></svg><span class="screen-reader-text">Previous Slide</span></a>';
+
+  var carousel_gallery_main = {
+    adaptiveHeight: true,
+    asNavFor: '.rc-carousel--gallery__thumbs',
+    autoplay: true,
+    nextArrow: carousel_next_arrow,
+    prevArrow: carousel_prev_arrow,
+    slidesToShow: 1
+  }
+
+  var carousel_gallery_thumbs = {
+    adaptiveHeight: true,
+    arrows: false,
+    asNavFor: '.rc-carousel--gallery__main',
+    focusOnSelect: true,
+    mobileFirst: true,
+    nextArrow: carousel_next_arrow,
+    prevArrow: carousel_prev_arrow,
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 5
+        }
+      }
+    ]
+  }
+
+  // Carousel Caches
+  var $slick_gallery = $( '.rc-carousel--gallery__main' );
+  var $slick_thumbs = $( '.rc-carousel--gallery__thumbs' );
+
+  // Carousel Inits
+  $slick_gallery.slick( carousel_gallery_main );
+  $slick_thumbs.slick( carousel_gallery_thumbs );
+
+});
