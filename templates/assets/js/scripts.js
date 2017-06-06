e53c78d3e4ac593d1e11f81277754b7cdcbf7668
James Drawneek <jmdrawneek@googlemail.com>
2017-06-06 10:14:37 +0100

    Added carousel copy for each design. Fixed json error for logos page.

--- templates/assets/js/scripts.js
+++ templates/assets/js/scripts.js
@@ -822,34 +822,6 @@ $(function () {
 
 /**
  *
- * File image-galleries.js.
- *
- */
-
-$(function () {
-
-  // Carousel Settings
-  var carousel_next_arrow = '<a href="#" class="interactive--navigation interactive--navigation--next"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35.5 64" aria-labelledby="svg-arrow-right" role="img"><title id="svg-arrow-right" lang="en">arrow-right</title><path d="M34.5 34.5L6 63c-.7.7-1.6 1-2.5 1s-1.8-.3-2.5-1c-1.4-1.4-1.4-3.6 0-5l26-26L1 6C-.4 4.6-.4 2.4 1 1S4.6-.4 6 1l28.5 28.5c1.4 1.4 1.4 3.6 0 5z"></path></svg><span class="screen-reader-text">Next Slide</span></a>',
-      carousel_prev_arrow = '<a href="#" class="interactive--navigation interactive--navigation--prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35.5 64" aria-labelledby="svg-arrow-left" role="img"><title id="svg-arrow-left" lang="en">arrow-left</title><path d="M1 29.5L29.5 1c1.4-1.4 3.6-1.4 5 0s1.4 3.6 0 5l-26 26 26 26c1.4 1.4 1.4 3.6 0 5-.7.7-1.6 1-2.5 1-.9 0-1.8-.3-2.5-1L1 34.5c-1.3-1.4-1.3-3.6 0-5z"></path></svg><span class="screen-reader-text">Previous Slide</span></a>';
-
-  var carousel_gallery_main = {
-    adaptiveHeight: false,
-    autoplay: true,
-    nextArrow: carousel_next_arrow,
-    prevArrow: carousel_prev_arrow,
-    slidesToShow: 1
-  }
-
-  // Carousel Caches
-  var $slick_gallery = $( '.rc-carousel--gallery__main' );
-
-  // Carousel Inits
-  $slick_gallery.slick( carousel_gallery_main );
-
-});
-
-/**
- *
  * File maps.js.
  *
  */
@@ -1097,3 +1069,31 @@ function initMap() {
   var infobox_first_10 = create_infobox( document.getElementById( 'infobox_first_10' ), marker_first_10, map_first_10 );
 }
 
+
+/**
+ *
+ * File image-galleries.js.
+ *
+ */
+
+$(function () {
+
+  // Carousel Settings
+  var carousel_next_arrow = '<a href="#" class="interactive--navigation interactive--navigation--next"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35.5 64" aria-labelledby="svg-arrow-right" role="img"><title id="svg-arrow-right" lang="en">arrow-right</title><path d="M34.5 34.5L6 63c-.7.7-1.6 1-2.5 1s-1.8-.3-2.5-1c-1.4-1.4-1.4-3.6 0-5l26-26L1 6C-.4 4.6-.4 2.4 1 1S4.6-.4 6 1l28.5 28.5c1.4 1.4 1.4 3.6 0 5z"></path></svg><span class="screen-reader-text">Next Slide</span></a>',
+      carousel_prev_arrow = '<a href="#" class="interactive--navigation interactive--navigation--prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35.5 64" aria-labelledby="svg-arrow-left" role="img"><title id="svg-arrow-left" lang="en">arrow-left</title><path d="M1 29.5L29.5 1c1.4-1.4 3.6-1.4 5 0s1.4 3.6 0 5l-26 26 26 26c1.4 1.4 1.4 3.6 0 5-.7.7-1.6 1-2.5 1-.9 0-1.8-.3-2.5-1L1 34.5c-1.3-1.4-1.3-3.6 0-5z"></path></svg><span class="screen-reader-text">Previous Slide</span></a>';
+
+  var carousel_gallery_main = {
+    adaptiveHeight: false,
+    autoplay: true,
+    nextArrow: carousel_next_arrow,
+    prevArrow: carousel_prev_arrow,
+    slidesToShow: 1
+  }
+
+  // Carousel Caches
+  var $slick_gallery = $( '.rc-carousel--gallery__main' );
+
+  // Carousel Inits
+  $slick_gallery.slick( carousel_gallery_main );
+
+});
