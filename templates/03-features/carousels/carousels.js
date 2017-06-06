f777cc663c0305ddbd879d786dd6531dbb966912
James Drawneek <jmdrawneek@googlemail.com>
2017-06-06 10:02:35 +0100

    https://trello.com/c/tkLi5m8U/151-carousel-background-images-to-fix Split carousels onto different pages per layout.

--- templates/03-features/carousels/carousels.js
+++ templates/03-features/carousels/carousels.js
@@ -14,7 +14,7 @@ $(function () {
       carousel_prev_arrow = '<a href="#" class="interactive--navigation interactive--navigation--prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35.5 64" aria-labelledby="svg-arrow-left" role="img"><title id="svg-arrow-left" lang="en">arrow-left</title><path d="M1 29.5L29.5 1c1.4-1.4 3.6-1.4 5 0s1.4 3.6 0 5l-26 26 26 26c1.4 1.4 1.4 3.6 0 5-.7.7-1.6 1-2.5 1-.9 0-1.8-.3-2.5-1L1 34.5c-1.3-1.4-1.3-3.6 0-5z"></path></svg><span class="screen-reader-text">Previous Slide</span></a>';
 
   var carousel_default = {
-    adaptiveHeight: true,
+    adaptiveHeight: false,
     dots: true,
     dotsClass: 'rc-carousel__dots list--blank list--align',
     autoplay: true,
@@ -24,7 +24,7 @@ $(function () {
     speed: 400,
     customPaging: function( slider, i ) {
       var thumb = $( slider.$slides[i] ).data( 'thumb' );
-      return '<a href="#" class="' + thumb + '"><span class="screen-reader-text">' + i + '</span></a>';
+      return '<button class="' + thumb + '"><span class="screen-reader-text">' + i + '</span></button>';
     }
   }
 
--- templates/assets/js/scripts.js
+++ templates/assets/js/scripts.js
@@ -305,6 +305,56 @@ $(function() {
 
 /**
  *
+ * File sliders.js.
+ *
+ */
+
+
+/**
+ * Checks if document is ready
+ * @param  {Function} fn Whatever is passed in the ready function 
+ */
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+
+
+
+
+
+// @todo - Will live in scripts file
+// Native document ready
+ready( function(  ) {
+
+	// Get element
+	var range = document.getElementById( 'rc-slider-demo' );
+
+	// Create Slider
+	noUiSlider.create( range, {
+		start: [ 0 ],
+		connect: [true, false],
+		behaviour: 'tap-drag', 
+		step: 10,
+		range: {
+			'min': 0,
+			'max': 100
+		},
+		pips: {
+			mode: 'steps',
+			stepped: true,
+			density: 2
+		}
+	});
+
+
+} );
+/**
+ *
  * File Progress.js.
  *
  */
@@ -435,56 +485,6 @@ $(function () {
 
 
 });
-/**
- *
- * File sliders.js.
- *
- */
-
-
-/**
- * Checks if document is ready
- * @param  {Function} fn Whatever is passed in the ready function 
- */
-function ready(fn) {
-  if (document.readyState != 'loading'){
-    fn();
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
-  }
-}
-
-
-
-
-
-
-// @todo - Will live in scripts file
-// Native document ready
-ready( function(  ) {
-
-	// Get element
-	var range = document.getElementById( 'rc-slider-demo' );
-
-	// Create Slider
-	noUiSlider.create( range, {
-		start: [ 0 ],
-		connect: [true, false],
-		behaviour: 'tap-drag', 
-		step: 10,
-		range: {
-			'min': 0,
-			'max': 100
-		},
-		pips: {
-			mode: 'steps',
-			stepped: true,
-			density: 2
-		}
-	});
-
-
-} );
 /**
  *
  * File Tabs.js.
@@ -797,7 +797,7 @@ $(function () {
       carousel_prev_arrow = '<a href="#" class="interactive--navigation interactive--navigation--prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35.5 64" aria-labelledby="svg-arrow-left" role="img"><title id="svg-arrow-left" lang="en">arrow-left</title><path d="M1 29.5L29.5 1c1.4-1.4 3.6-1.4 5 0s1.4 3.6 0 5l-26 26 26 26c1.4 1.4 1.4 3.6 0 5-.7.7-1.6 1-2.5 1-.9 0-1.8-.3-2.5-1L1 34.5c-1.3-1.4-1.3-3.6 0-5z"></path></svg><span class="screen-reader-text">Previous Slide</span></a>';
 
   var carousel_default = {
-    adaptiveHeight: true,
+    adaptiveHeight: false,
     dots: true,
     dotsClass: 'rc-carousel__dots list--blank list--align',
     autoplay: true,
@@ -807,7 +807,7 @@ $(function () {
     speed: 400,
     customPaging: function( slider, i ) {
       var thumb = $( slider.$slides[i] ).data( 'thumb' );
-      return '<a href="#" class="' + thumb + '"><span class="screen-reader-text">' + i + '</span></a>';
+      return '<button class="' + thumb + '"><span class="screen-reader-text">' + i + '</span></button>';
     }
   }
 
@@ -817,6 +817,34 @@ $(function () {
   // Carousel Inits
   $slick_hero.slick( carousel_default );
 
+
+});
+
+/**
+ *
+ * File image-galleries.js.
+ *
+ */
+
+$(function () {
+
+  // Carousel Settings
+  var carousel_next_arrow = '<a href="#" class="interactive--navigation interactive--navigation--next"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35.5 64" aria-labelledby="svg-arrow-right" role="img"><title id="svg-arrow-right" lang="en">arrow-right</title><path d="M34.5 34.5L6 63c-.7.7-1.6 1-2.5 1s-1.8-.3-2.5-1c-1.4-1.4-1.4-3.6 0-5l26-26L1 6C-.4 4.6-.4 2.4 1 1S4.6-.4 6 1l28.5 28.5c1.4 1.4 1.4 3.6 0 5z"></path></svg><span class="screen-reader-text">Next Slide</span></a>',
+      carousel_prev_arrow = '<a href="#" class="interactive--navigation interactive--navigation--prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35.5 64" aria-labelledby="svg-arrow-left" role="img"><title id="svg-arrow-left" lang="en">arrow-left</title><path d="M1 29.5L29.5 1c1.4-1.4 3.6-1.4 5 0s1.4 3.6 0 5l-26 26 26 26c1.4 1.4 1.4 3.6 0 5-.7.7-1.6 1-2.5 1-.9 0-1.8-.3-2.5-1L1 34.5c-1.3-1.4-1.3-3.6 0-5z"></path></svg><span class="screen-reader-text">Previous Slide</span></a>';
+
+  var carousel_gallery_main = {
+    adaptiveHeight: false,
+    autoplay: true,
+    nextArrow: carousel_next_arrow,
+    prevArrow: carousel_prev_arrow,
+    slidesToShow: 1
+  }
+
+  // Carousel Caches
+  var $slick_gallery = $( '.rc-carousel--gallery__main' );
+
+  // Carousel Inits
+  $slick_gallery.slick( carousel_gallery_main );
 
 });
 
@@ -1069,31 +1097,3 @@ function initMap() {
   var infobox_first_10 = create_infobox( document.getElementById( 'infobox_first_10' ), marker_first_10, map_first_10 );
 }
 
-
-/**
- *
- * File image-galleries.js.
- *
- */
-
-$(function () {
-
-  // Carousel Settings
-  var carousel_next_arrow = '<a href="#" class="interactive--navigation interactive--navigation--next"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35.5 64" aria-labelledby="svg-arrow-right" role="img"><title id="svg-arrow-right" lang="en">arrow-right</title><path d="M34.5 34.5L6 63c-.7.7-1.6 1-2.5 1s-1.8-.3-2.5-1c-1.4-1.4-1.4-3.6 0-5l26-26L1 6C-.4 4.6-.4 2.4 1 1S4.6-.4 6 1l28.5 28.5c1.4 1.4 1.4 3.6 0 5z"></path></svg><span class="screen-reader-text">Next Slide</span></a>',
-      carousel_prev_arrow = '<a href="#" class="interactive--navigation interactive--navigation--prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35.5 64" aria-labelledby="svg-arrow-left" role="img"><title id="svg-arrow-left" lang="en">arrow-left</title><path d="M1 29.5L29.5 1c1.4-1.4 3.6-1.4 5 0s1.4 3.6 0 5l-26 26 26 26c1.4 1.4 1.4 3.6 0 5-.7.7-1.6 1-2.5 1-.9 0-1.8-.3-2.5-1L1 34.5c-1.3-1.4-1.3-3.6 0-5z"></path></svg><span class="screen-reader-text">Previous Slide</span></a>';
-
-  var carousel_gallery_main = {
-    adaptiveHeight: false,
-    autoplay: true,
-    nextArrow: carousel_next_arrow,
-    prevArrow: carousel_prev_arrow,
-    slidesToShow: 1
-  }
-
-  // Carousel Caches
-  var $slick_gallery = $( '.rc-carousel--gallery__main' );
-
-  // Carousel Inits
-  $slick_gallery.slick( carousel_gallery_main );
-
-});
