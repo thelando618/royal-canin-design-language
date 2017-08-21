1195035d3794413c9303d7b9e206dee5a7ba9b46
James Drawneek <jmdrawneek@googlemail.com>
2017-08-21 15:14:26 +0100

    Changed svg generation to use older method with no callback for better compatibility.

--- gulp/tasks/svgo.js
+++ gulp/tasks/svgo.js
@@ -15,12 +15,7 @@ function recolour(file, t, variants, dist) {
     // Convert the file binary into a string and find/replace the fill with each variant.
     let svg = file._contents.toString().replace(rgx, 'fill:' + variant.colour);
 
-    fs.writeFileSync(dist + fileName[0] + variant.suffix + '.' + fileName[1], svg, function(err) {
-      if (err) {
-        console.log(err);
-        throw err;
-      }
-    });
+    fs.writeFileSync(dist + fileName[0] + variant.suffix + '.' + fileName[1], svg);
   });
 }
 
