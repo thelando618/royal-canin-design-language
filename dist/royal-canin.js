4ba52494baea35ae70eaf4f97b144c616956aa1c
James Drawneek <jmdrawneek@googlemail.com>
2017-09-28 10:19:32 +0100

    Added updated assets.

--- dist/royal-canin.js
+++ dist/royal-canin.js
@@ -4008,6 +4008,39 @@ RCDL.utilities.svgAnimation = function (interactiveSvg) {
 };
 
 RCDL.ready(RCDL.utilities.svgAnimation('[data-js-import-interactive-svg]'));
+RCDL.features.alerts = {
+  /**
+   * Add the default functionality for alerts. Remove alert if close button clicked.
+   * Store this decision in the local session and hide the alerts if they've already been closed.
+   *
+   * @param {String|Array} selectors
+   * Either a css selector or an array or selectors.
+   */
+  init: function (selectors) {
+
+    if (typeof selectors === 'string') {
+      selectors = [selectors];
+    }
+
+    selectors.forEach(function (selector) {
+      var alerts = document.querySelectorAll(selector);
+
+      alerts.forEach(function (alert) {
+
+        if (sessionStorage.getItem(String(alert.innerHTML)) !== null) {
+          alert.remove();
+        }
+
+        alert.querySelector('.alert__close').addEventListener('click', function (event) {
+          sessionStorage.setItem(String(event.currentTarget.parentNode.innerHTML), String(event.currentTarget.parentNode.innerHTML));
+          event.currentTarget.parentNode.remove();
+        });
+      });
+    });
+  }
+};
+
+RCDL.ready(RCDL.features.alerts.init(['[role="alert"]']));
 /**
  * Takes a selector and converts into a carousel using the tiny-slider library.
  * @type {{init: RCDL.features.Carousel.init, create: RCDL.features.Carousel.create}}
