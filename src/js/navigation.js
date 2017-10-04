fc3cfc63e3b86f87379b87df81ca8e2bd0d825a6
Milly Billins <milly@first10.co.uk>
2017-10-04 11:54:44 +0100

    Ported main navigation over from GCW

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -137,3 +137,214 @@ RCWDL.navigation.burgerToggle = function (triggerSelector, targetSelector) {
 };
 
 RCWDL.ready(RCWDL.navigation.burgerToggle('[data-js-animate-svg]', '[data-js-animate-svg-target]'));
+
+/** 
+ * Open and close main navigation
+ * 
+ * @param {String} mainNavSelector Css selector for targeting the main navigation.
+ *
+ * @param {String} mainNavTogglerSelector Css selector for targeting the toggler.
+ */
+RCWDL.navigation.toggleNavigation = function (mainNavSelector, mainNavTogglerSelector) {
+  'use strict';
+
+  RCWDL.ready(RCWDL.utilities.triggerAndTargetClassModifier.init('click', mainNavTogglerSelector, '[data-js-trigger]', '.open', null));
+
+  var mainNavWrapper = document.querySelector(mainNavSelector).parentNode;
+  var mainNavTogglers = document.querySelectorAll(mainNavTogglerSelector);
+
+  mainNavTogglers.forEach(function (mainNavToggler) {
+    mainNavToggler.addEventListener('click', function () {
+      RCWDL.utilities.toggleClass(mainNavToggler.parentNode, 'active');
+
+      /**
+       * Prevent page scroll when nav is open
+       */
+      if (mainNavWrapper !== null) {
+        if (RCWDL.utilities.hasClass(mainNavWrapper, 'open')) {
+          document.body.style.overflow = 'hidden';
+        }
+        else {
+          document.body.style.overflow = '';
+        }
+      }
+    });
+  });
+};
+
+RCWDL.ready(RCWDL.navigation.toggleNavigation('.rc-main-navigation', '[data-js-trigger="main-nav"]'));
+
+/** 
+ * Open and close navigation sub items
+ * 
+ * Takes two selectors: 1) The main navigation div 2) The main navigation item selector
+ *
+ * @param {String} mainNavSelector
+ * Css selector.
+ *
+ * @param {String} mainNavItemSelector
+ * Css selector.
+ */
+RCWDL.navigation.openCloseItems = function (mainNavSelector, mainNavItemSelector) {
+  'use strict';
+
+  var mainNav = document.querySelector(mainNavSelector);
+  var mainNavItems = mainNav.querySelectorAll(mainNavItemSelector);
+  var triggerType = window.innerWidth > 800 ? 'mouseover' : 'click';
+
+  mainNavItems.forEach(function (mainNavItem) {
+    mainNavItem.addEventListener(triggerType, function () {
+
+      var nodeSiblings = RCWDL.utilities.getSiblings(mainNavItem.parentNode);
+
+      nodeSiblings.forEach(function (sibling) {
+        if (sibling !== mainNavItem.parentNode) {
+          RCWDL.utilities.removeClass(sibling, 'open');
+          RCWDL.utilities.removeClass(sibling, 'open--desktop');
+        }
+        else {
+          RCWDL.utilities.addClass(sibling, 'open');
+        }
+      });
+
+    });
+  });
+};
+
+RCWDL.ready(RCWDL.navigation.openCloseItems('.rc-main-navigation', '.rc-main-navigation__item-text'));
+
+/**
+ * Creates a dynamic breadcrumb within the mobile navigation
+ *
+ * @param {String} mainNavSelector Css selector for targeting the main navigation.
+ * 
+ * @param {String} mainNavBreadcrumbSelector Css selector for targeting the breadcrumb div.
+ * 
+ * @param {Array} mainNavBreadcrumbLevels The level descriptions for the breadcrumb e.g primary, secondary.
+ */
+RCWDL.navigation.breadcrumbs = function (mainNavSelector, mainNavBreadcrumbSelector, mainNavBreadcrumbLevels) {
+  'use strict';
+
+  var mainNavBreadcrumbs = document.querySelector(mainNavBreadcrumbSelector);
+  var mainNav = document.querySelector(mainNavSelector);
+  var mainNavItems = mainNav.querySelectorAll(mainNavSelector + '__item-text');
+
+  var levels = mainNavBreadcrumbLevels;
+
+  mainNavItems.forEach(function (mainNavItem) {
+    mainNavItem.addEventListener('click', function () {
+      var breadcrumb = mainNavItem.cloneNode(true);
+      var breadcrumbClass = '';
+
+      levels.forEach(function (level) {
+        var currentLevel = level;
+        if (RCWDL.utilities.hasClass(mainNavItem.parentNode.parentNode, 'rc-main-navigation__' + currentLevel)) {
+          breadcrumbClass = currentLevel;
+        }
+      });
+
+      mainNavBreadcrumbs.innerHTML += '<li class="rc-main-navigation__breadcrumb-item ' + breadcrumbClass + '">' + breadcrumb.innerText + '</li>';
+
+    });
+  });
+
+  mainNavBreadcrumbs.addEventListener('click', function (e) {
+
+    levels.forEach(function (level) {
+      var currentLevel = level;
+
+      if (e.target.nodeName === 'LI' && RCWDL.utilities.hasClass(e.target, currentLevel)) {
+
+        var allNodes = mainNavBreadcrumbs.querySelectorAll('li');
+        var currentNode = e.currentTarget.querySelector('.' + currentLevel);
+        var currentNav = mainNav.querySelectorAll(mainNavSelector + '__' + currentLevel);
+
+        currentNav.forEach(function (currentNavItem) {
+          var currentNavChildren = currentNavItem.querySelectorAll('.haschild');
+
+          currentNavChildren.forEach(function (currentNavChild) {
+            if (RCWDL.utilities.hasClass(currentNavChild, 'open')) {
+              RCWDL.utilities.removeClass(currentNavChild, 'open');
+            }
+          });
+        });
+
+        if (currentLevel === 'primary') {
+          allNodes.forEach(function (node) {
+            node.remove();
+          });
+        }
+        else {
+          currentNode.remove();
+        }
+      }
+    });
+  });
+};
+
+if (window.innerWidth < 800) {
+  RCWDL.ready(RCWDL.navigation.breadcrumbs('.rc-main-navigation', '.rc-main-navigation__breadcrumb-list', ['primary', 'secondary']));
+}
+
+/**
+ * Opens/closes extra content in navigation and smooth scrolls to it
+ *
+ * @param {String} buttonSelector Css selector for the trigger button.
+ * 
+ * @param {String} buttonTargetSelector Css selector for the target div.
+ * 
+ * @param {String} mainNavSelector Css selector for the main navigation.
+ */
+RCWDL.navigation.expandAndScrollTo = function (buttonSelector, buttonTargetSelector, mainNavSelector) {
+  'use strict';
+
+  var buttonTrigger = document.querySelector(buttonSelector);
+
+  buttonTrigger.addEventListener('click', function () {
+    var buttonTarget = document.querySelector(buttonTargetSelector);
+    var mainNavWrapper = document.querySelector(mainNavSelector).parentNode;
+
+    RCWDL.utilities.toggleClass(buttonTarget, 'open');
+    RCWDL.utilities.toggleClass(this, 'open');
+
+    function scrollTo(element, to, duration) {
+
+      Math.easeInOutQuad = function (t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) {
+          return c / 2 * t * t + b;
+        }
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+      };
+
+      var start = element.scrollTop;
+      var change = to - start;
+      var currentTime = 0;
+      var increment = 20;
+
+      var animateScroll = function () {
+        currentTime += increment;
+        var val = Math.easeInOutQuad(currentTime, start, change, duration);
+        element.scrollTop = val;
+        if (currentTime < duration) {
+          setTimeout(animateScroll, increment);
+        }
+      };
+      animateScroll();
+    }
+
+    if (RCWDL.utilities.hasClass(this, 'open')) {
+      scrollTo(mainNavWrapper, mainNavWrapper.scrollHeight, 500);
+      this.querySelector('[data-js-text]').innerText = 'Less Information';
+    }
+    else {
+      scrollTo(mainNavWrapper, mainNavWrapper.top, 500);
+      this.querySelector('[data-js-text]').innerText = 'More Information';
+    }
+  });
+};
+
+if (window.innerWidth < 800) {
+  RCWDL.ready(RCWDL.navigation.expandAndScrollTo('[data-js-trigger="more-info"]', '[data-js-target="more-info"]', '.rc-main-navigation'));
+}
